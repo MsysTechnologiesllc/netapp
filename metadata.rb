94217e102fa9ae171febf05144cd43ebeba00b34
Mukta A <mukta.aphale@clogeny.com>
2014-06-24 15:47:30 +0530

    First draft of the netapp cookbook

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'netapp'
+maintainer       'Mukta Aphale'
+maintainer_email 'mukta.aphale@clogeny.com'
+license          'All rights reserved'
+description      'Installs/Configures netapp'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: netapp
+# Recipe:: default
+#
+# Copyright 2014, Mukta Aphale
+#
+# All rights reserved - Do Not Redistribute
+#
